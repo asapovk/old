068c2e8a573dade3863a0f611cc8a55371fabf91
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-07-03 12:37:53 +0300

    Table columns children now have a key

--- src/components/Table/TableColumns.jsx
+++ src/components/Table/TableColumns.jsx
@@ -15,7 +15,7 @@ export default class TableColumns extends React.Component {
                     key={column.dataIndex}
                     className={`ui-table-content-body-row-column ` + column.dataIndex}
                     style={column.width ? { flexBasis: column.width } : { flex: 1 }}
-                    children={column.render.apply(scope, [data, data[column.dataIndex]])}
+                    children={column.render.apply(scope, [data, data[column.dataIndex], key])}
                 />
             )
         });
