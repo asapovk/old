59dc512ebd28b2a26a71190537dee91f5335503e
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-07-30 17:54:41 +0300

    Props for EditForm

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -41,7 +41,7 @@ class Table extends React.Component {
                         ))}
                     </div>
                     <div className='ui-table-content-body'>
-                        {newItem && <EditForm />}
+                        {newItem && <EditForm {...this.props} />}
                         {this.props.data.map((row, index) => (
                             <TableRow
                                 {...this.props}
