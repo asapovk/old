97d9900652b665852a94057e874ea80aebe5e2f1
Илья <i.trikoz@abr-region.ru>
2018-08-08 16:58:34 +0300

    add flexbox and portal

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { Table as Table } from './Table'
 export { TextField as TextField } from './TextField'
 export { Viewport as Viewport } from './Viewport'
+
+export { default as Flexbox } from 'react-flexbox';
+export { default as Portal } from 'react-portal';
