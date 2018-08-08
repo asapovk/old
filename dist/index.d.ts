42f69f97e118198c13116853cf3ab52fa856173f
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-08-08 16:47:14 +0300

    Button export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { Table as Table } from './Table';
 export { TextField as TextField } from './TextField';
 export { Viewport as Viewport } from './Viewport';
+export { Button as Button } from './Button';
