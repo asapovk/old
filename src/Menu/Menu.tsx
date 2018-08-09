fb424e4be1ca06b94c343ff24cdca26fc68c1fe2
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-08-09 11:39:30 +0300

    Menu structure

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -20,13 +20,16 @@ class Menu extends React.Component<TableProps> {
     componentWillMount() {
 
     }
+
     render() {
         return (
             <div className='ui-menu'>
-                <div className='menu'>
-                    <div className='nav'>{this.props.children}</div>
-                    {/* <button className='add' onClick={_ => AppActions.menuActions.add()}><Icon type='add' /></button> */}
+                {/* <div className='ui-menu-header'>{this.props.children.Header}</div> */}
+                <div className='ui-menu-interactions'>
+                    <div className='ui-menu-interactions-tools'>{this.props.toolsLeft}</div>
+                    {/* <div className='ui-menu-interactions-nav'>{this.props.children.Nav}</div> */}
                 </div>
+                <div className='ui-menu-tools'>{this.props.toolsLeft}</div>
             </div>
         )
     }
