e6c0fa0e4ed4c758d1f0e1e82cb5bb51394ea30e
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-08-10 09:44:06 +0300

    Menu exports

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { Table as Table } from './Table'
 export { TextField as TextField } from './TextField'
 export { Viewport as Viewport } from './Viewport'
 export { Button as Button } from './Button'
+export { Menu as Menu } from './Menu'
+export { Nav as Nav } from './Menu'
 
 export { default as Portal } from 'react-portal';
