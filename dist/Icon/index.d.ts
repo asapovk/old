01d531fce9a94511be202a794c317d880f9ee9f6
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-08-13 09:26:04 +0300

    Viewport height fix

--- dist/Icon/index.d.ts
+++ dist/Icon/index.d.ts
@@ -1,2 +1,8 @@
+export declare const icons: {
+    'add': string;
+    'more': string;
+    'pin': string;
+    'close': string;
+};
 declare const _default: (props: any) => JSX.Element;
 export default _default;
