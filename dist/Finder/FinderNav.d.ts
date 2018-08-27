385df9d45f7df04d643f68dba198938db2f6efeb
Илья <i.trikoz@abr-region.ru>
2018-08-27 10:45:33 +0300

    fix

--- dist/Finder/FinderNav.d.ts
+++ dist/Finder/FinderNav.d.ts
@@ -3,13 +3,12 @@ interface FinderNavProps {
     label: string;
     filter?: boolean;
     filterPlaceholder?: string;
-    render?: (children: any, filter: any, level: any, filterPlaceholder: any, updateChildren: any, update: any) => void;
+    render?: (children: any, filter: any, level: any, filterPlaceholder: any, updateChildren: any) => void;
     level?: number;
 }
 declare class FinderNav extends React.Component<FinderNavProps> {
     constructor(props: any);
-    expand(children: any, update: any): void;
-    updateChildren(): void;
+    expand(): void;
     render(): JSX.Element;
 }
 export default FinderNav;
