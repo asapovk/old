cba21b6b243139a3ea09d269cae6ca8c3f9d16dc
Dmitry Beltyukov <d.beltukov@abr-region.ru>
2018-08-27 11:31:08 +0300

    Finder remove children from state

--- src/Finder/Finder.tsx
+++ src/Finder/Finder.tsx
@@ -22,21 +22,12 @@ class Finder extends React.Component<FinderProps> {
 
     state = {
         menues: [] as {
-            childrens: any
             filter: boolean
             filterValue: string
             filterPlaceholder: string
-            updateChildren: () => void
+            getChildren: () => void,
+            level: number
         }[]
-    }
-
-    componentWillReceiveProps() {
-        const menues = this.state.menues.map(menu => {
-            menu.childrens = menu.updateChildren()
-            return menu
-        });
-        console.log(menues);
-        //this.setState({ menu: menues });
     }
 
     passFinderProps(children, level) {
@@ -49,14 +40,14 @@ class Finder extends React.Component<FinderProps> {
         });
     }
 
-    setMenues(children, filter, level, filterPlaceholder, updateChildren) {
+    setMenues(filter, level, filterPlaceholder, getChildren) {
         let menues = this.state.menues;
         menues[level] = {
-            childrens: this.passFinderProps(children, level),
             filter: filter,
             filterValue: '',
             filterPlaceholder: filterPlaceholder,
-            updateChildren: updateChildren
+            getChildren: getChildren,
+            level: level
         };
         menues.length = level + 1;
         this.setState({ menu: menues });
@@ -71,13 +62,12 @@ class Finder extends React.Component<FinderProps> {
     render() {
 
         const { filter, filterPlaceholder } = this.props;
+
         const MenuesTSX = (
             this.state.menues.map((menu, index) => (
                 <div className='ui-finder-menu' key={index}>
                     {menu.filter && <FinderFilter level={index} filterChange={this.filterChange} placeholder={menu.filterPlaceholder} />}
-                    <div className='ui-finder-menu-items'>{menu.childrens && menu.childrens.filter(
-                        child => child.props.label.includes(menu.filterValue)
-                    )}</div>
+                    <div className='ui-finder-menu-items'>{this.passFinderProps(menu.getChildren(), menu.level)}</div>
                 </div>
             ))
         )
