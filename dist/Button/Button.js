568e3d3a11dedc113a66731455da33b425d7cfae
Илья <i.trikoz@abr-region.ru>
2018-09-07 09:35:34 +0300

    build

--- dist/Button/Button.js
+++ dist/Button/Button.js
@@ -21,20 +21,26 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importStar(require("react"));
+var index_1 = require("../index");
 var Button = /** @class */ (function (_super) {
     __extends(Button, _super);
     function Button() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Button.prototype.render = function () {
-        var _a = this.props, decoration = _a.decoration, labelCase = _a.labelCase, onClick = _a.onClick, label = _a.label, children = _a.children, className = _a.className, style = _a.style;
+        var _a = this.props, decoration = _a.decoration, labelCase = _a.labelCase, onClick = _a.onClick, label = _a.label, children = _a.children, className = _a.className, style = _a.style, loading = _a.loading;
         var classes = 'ui-button';
         if (decoration == 'none')
             classes += ' btn-strip';
         if (labelCase == 'upper')
             classes += ' uppercase';
+        if (loading)
+            classes += ' loading';
         classes += className ? ' ' + className : '';
-        return (react_1.default.createElement("button", { className: classes, onClick: onClick, style: style }, label || children));
+        return (react_1.default.createElement("button", { className: classes, onClick: onClick, style: style },
+            react_1.default.createElement("span", null, label || children),
+            loading && (react_1.default.createElement(index_1.Spin, null,
+                react_1.default.createElement(index_1.Icon, { type: "sync" })))));
     };
     return Button;
 }(react_1.Component));
