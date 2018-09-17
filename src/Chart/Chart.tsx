91cb681f7f0e5b2e894058be5b3efcd90c33e7c7
Илья <i.trikoz@abr-region.ru>
2018-09-17 17:09:21 +0300

    1

---
+++ src/Chart/Chart.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Icon } from '../Icon';
+
+interface Props {
+
+}
+
+class Chart extends React.Component<Props> {
+    render() {
+        return (
+            <div></div>
+
+        )
+    }
+}
+
+export default Chart;
