463da7b71b289f2d4631487f4fb0ab1360fb3d9d
Awesome <jleed@me.com>
2018-10-09 13:06:30 +0300

    build

--- dist/Icon/Icon.d.ts
+++ dist/Icon/Icon.d.ts
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from 'react';
-export declare type IconType = 'add' | 'more' | 'pin' | 'close' | 'search' | 'right' | 'left' | 'down' | 'up' | 'check' | 'sync' | 'filter' | 'settings' | 'chart' | 'calendar' | 'spin' | 'smorodina';
+export declare type IconType = 'add' | 'more' | 'pin' | 'close' | 'search' | 'right' | 'left' | 'down' | 'up' | 'check' | 'sync' | 'filter' | 'settings' | 'chart' | 'calendar' | 'spin' | 'smorodina' | 'clock';
 export declare const svgIconPath: Record<IconType, string>;
 interface Props {
     style?: CSSProperties;
