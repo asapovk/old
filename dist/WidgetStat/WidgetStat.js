d6ed2115b6c4a11c68c58f1607cec6364ed9b042
Alexander <delle@alexanders-mbp.abrr.local>
2018-12-06 12:49:29 +0300

    ws

--- dist/WidgetStat/WidgetStat.js
+++ dist/WidgetStat/WidgetStat.js
@@ -43,7 +43,7 @@ var WidgetStat = /** @class */ (function (_super) {
                         react_1.default.createElement(__1.Flexbox, { className: "ui-widgetstat-subtitle2", style: styles.widgetStat.subtitleStyle }, subtitle2))),
                 react_1.default.createElement(__1.Flexbox, { pt: 10, alignItems: "flex-end" },
                     react_1.default.createElement(__1.Flexbox, { pr: 10, flex: 1, className: "ui-widgetstat-value1", style: styles.widgetStat.valueStyle }, value1),
-                    typeof onClick === "function" ? react_1.default.createElement(__1.Button, { decoration: "none", className: "ui-widgetstat-button", onClick: onClick }, actionTitle || "Подробнее") : null)),
+                    typeof onClick === "function" ? react_1.default.createElement("div", { className: "ui-widgetstat-link", onClick: onClick }, actionTitle || "Подробнее") : null)),
             typeof children !== "undefined" ? (react_1.default.createElement(__1.Flexbox, { flexDirection: 'column', className: "ui-widgetstat-extra", style: styles.widgetStat.extraStyle }, children)) : null)); }));
     };
     return WidgetStat;
