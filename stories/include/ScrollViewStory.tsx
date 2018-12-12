9844e6788a85d1864c3a740d4e6decad4af2d6e7
Awesome <jleed@me.com>
2018-12-12 17:59:39 +0300

    ScrollView story update

--- stories/include/ScrollViewStory.tsx
+++ stories/include/ScrollViewStory.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView } from '../../src/ScrollView';
 import useTheme from '../../src/hooks/useTheme';
+import { Title } from '../../src/Title';
 
 export default () => {
     const { theme } = useTheme();
@@ -15,14 +16,15 @@ export default () => {
                     }}
                     children={(
                         <>
-                            <h1 style={{ margin: 20 }}>Vertical scroll</h1>
+                            <Title style={{ padding: 40 }}>VERTICAL</Title>
                             <ScrollView horizontal>
                                 <div
                                     style={{
                                         width: 3000,
                                         background: 'linear-gradient(90deg, ' + theme.pale.rgba(0) + ', ' + theme.pale.rgba(1) + ')'
                                     }}
-                                    children={<h1 style={{ margin: 20 }}>Horizontal scroll</h1>}
+                                    children={<Title style={{ padding: 40 }}>HORIZONTAL</Title>
+                                    }
                                 />
                             </ScrollView>
                         </>
