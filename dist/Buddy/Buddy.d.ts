87d9ac400b9139bcf9b00296d6e159294d9d4fc1
Alexander <delle@alexanders-mbp.abrr.local>
2018-12-12 10:24:16 +0300

    123

--- dist/Buddy/Buddy.d.ts
+++ dist/Buddy/Buddy.d.ts
@@ -1,47 +1,9 @@
-import { Component, CSSProperties } from 'react';
+import React, { CSSProperties } from 'react';
 export interface Props {
     style?: CSSProperties;
     size?: number;
-    defaultFace?: "happy" | "sad";
+    face?: "happy" | "sad" | "normal";
+    jumpy?: () => void;
 }
-declare class Buddy extends Component<Props> {
-    timeline: any;
-    buddy: HTMLElement;
-    buddyContainer: HTMLElement;
-    buddyMouth: HTMLElement;
-    buddyBrouwLeft: HTMLElement;
-    buddyBrouwRight: HTMLElement;
-    buddyEyes: HTMLCollection;
-    buddyBrouws: HTMLCollection;
-    PATH_STATES: any;
-    cursorPos: {
-        x: number;
-        y: number;
-    };
-    BUDDY_ID: string;
-    constructor(props: any);
-    init(): void;
-    /**
-     * Счастливый
-     */
-    makeHappy(instant?: boolean): void;
-    /**
-     * Обычный
-     */
-    makeNormal(instant?: boolean): void;
-    /**
-     * Грустный
-     */
-    makeSad(instant?: boolean): void;
-    /**
-     * Попрыгун
-     */
-    makeJumpy(repeat?: number): void;
-    stopAnimation(): void;
-    private setListenersEnabled;
-    private moveEyes;
-    componentDidMount(): void;
-    componentWillUnmount(): void;
-    render(): JSX.Element;
-}
-export default Buddy;
+declare const _default: React.ForwardRefExoticComponent<Props & React.RefAttributes<{}>>;
+export default _default;
