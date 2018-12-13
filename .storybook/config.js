7ab0008f869ddf34d3fab3334f7234e9308c86f4
Dmitry Beltyukov <beltdtr@gmail.com>
2018-12-13 14:34:37 +0300

    Config options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,17 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
 
-withOptions({
-  name: 'ABRR-UI',
-  showAddonPanel: true,
-  showSearchBox: false,
-  sortStoriesByKind: false,
-  sidebarAnimations: false,
-  selectedAddonPanel: undefined,
-  enableShortcuts: false,
-})
+addDecorator(
+  withOptions({
+    name: 'ABRR-UI',
+    showAddonPanel: true,
+    showSearchBox: false,
+    sortStoriesByKind: false,
+    sidebarAnimations: false,
+    selectedAddonPanel: undefined,
+    enableShortcuts: false,
+  })
+);
 
 function loadStories() {
   require('../stories');
