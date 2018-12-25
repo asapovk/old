0572bb66919c22ba3aeafbe50296276a9ef2e27b
Dmitry Beltyukov <beltdtr@gmail.com>
2018-12-25 13:45:25 +0300

    remove alias

--- showcase_workspace/showcase/webpack.config.ts
+++ showcase_workspace/showcase/webpack.config.ts
@@ -16,10 +16,7 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"],
-        alias: {
-            'webpack-dev-server/client': path.resolve(__dirname + '/node_modules/webpack-dev-server/client')
-        },
+        extensions: [".ts", ".tsx", ".js", ".json"]
     },
 
     module: {
