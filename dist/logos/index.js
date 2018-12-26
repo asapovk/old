8950ed76da954ef63e0447ed99a7d86de705bacf
Dmitry Beltyukov <beltdtr@gmail.com>
2018-12-26 12:57:37 +0300

    Logos export

---
+++ dist/logos/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mrg_1 = require("./mrg");
+exports.MrgLogo = mrg_1.default;
+var abr_1 = require("./abr");
+exports.AbrLogo = abr_1.default;
+var smorodina_1 = require("./smorodina");
+exports.SmorodinaLogo = smorodina_1.default;
---
+++ src/logos/index.ts
@@ -0,0 +1,3 @@
+export { default as MrgLogo } from './mrg';
+export { default as AbrLogo } from './abr';
+export { default as SmorodinaLogo } from './smorodina';
