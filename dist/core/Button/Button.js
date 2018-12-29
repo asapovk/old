59847c7f93281bc44a6b67c9bf73926fe6371baa
Alexander <delle@alexanders-mbp.abrr.local>
2018-12-29 13:30:55 +0300

    build

--- dist/core/Button/Button.js
+++ dist/core/Button/Button.js
@@ -8,7 +8,7 @@ var core_1 = require("@emotion/core");
 var useStyles_1 = __importDefault(require("./useStyles"));
 var index_1 = require("../index");
 exports.default = (function (props) {
-    var labelCase = props.labelCase, label = props.label, children = props.children, style = props.style, loading = props.loading, decoration = props.decoration, disabled = props.disabled, size = props.size, inversion = props.inversion;
+    var labelCase = props.labelCase, label = props.label, children = props.children, style = props.style, loading = props.loading, decoration = props.decoration, disabled = props.disabled, size = props.size, inversion = props.inversion, className = props.className;
     var styles = useStyles_1.default(size, loading, disabled, labelCase, decoration, inversion);
     var onClick = function (event) {
         if (!props.disabled && !props.loading) {
@@ -16,7 +16,7 @@ exports.default = (function (props) {
             props.onClick && props.onClick();
         }
     };
-    return (core_1.jsx("button", { css: styles, style: style, onClick: function (event) { return onClick(event); } },
+    return (core_1.jsx("button", { css: styles, className: className, style: style, onClick: function (event) { return onClick(event); } },
         core_1.jsx("span", null, label || children),
         loading && (core_1.jsx(index_1.Spin, null,
             core_1.jsx(index_1.Icon, { type: "sync" })))));
