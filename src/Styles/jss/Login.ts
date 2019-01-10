f6652ff28e2d786b3a3f5ac39822be18c224446b
Alexander <delle@alexanders-mbp.abrr.local>
2019-01-10 14:41:40 +0300

    remove login styles from styles

--- src/Styles/jss/Login.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from "react";
-import { ThemeInterface } from "..";
-
-export default (theme: ThemeInterface) => {
-    return {
-        textColor: theme.text.rgb,
-        textShadow: `0px 1px 6px rgba(${theme.shadow.rgb}, 0.5)`
-    }
-}
