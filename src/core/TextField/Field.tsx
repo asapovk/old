60eb2a67ac15e6006f7585b940586889ad71f918
Dmitry Beltyukov <beltdtr@gmail.com>
2019-01-16 11:15:16 +0300

    onEnter TexField props

--- src/core/TextField/Field.tsx
+++ src/core/TextField/Field.tsx
@@ -21,6 +21,7 @@ export default forwardRef((props: Types.FieldProps, ref) => {
             defaultValue: props.defaultValue,
             value: props.value,
             onChange: onChange,
+            onEnter: props.onEnter,
             disabled: props.disabled,
             placeholder: props.placeholder,
             css: props.styles.field,
