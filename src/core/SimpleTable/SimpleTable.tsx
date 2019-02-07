636224f3da990822763b1a941e3f0d6769641d9e
Alexander <delle@alexanders-mbp.abrr.local>
2019-02-07 16:44:49 +0300

    table ref

--- src/core/SimpleTable/SimpleTable.tsx
+++ src/core/SimpleTable/SimpleTable.tsx
@@ -8,37 +8,39 @@ import TablePagination from './components/TablePagination';
 import TableRow from './components/TableRow';
 
 export default (props: Types.Props) => {
+    const { data, noDataComponent, pagination, hideHeaders, columns } = props;
     const styles = createStyles();
     const [currentPage, setCurrentPage] = useState(1);
 
-    let pageData = props.data;
-    if (props.pagination) {
-        const { pageSize } = props.pagination;
-        pageData = props.data
-            .filter((_, i) => pageSize * currentPage >= (i + 1) && i >= pageSize * currentPage - pageSize);
-    }
-
-    if (!props.data || props.data.length <= 0) {
+    if (!data || data.length <= 0) {
         return (
-            props.noDataComponent
-                ? props.noDataComponent
+            noDataComponent
+                ? noDataComponent
                 : <div css={styles.noDataContainer}>Нет данных</div>
         )
     }
 
+    let pageData = data;
+
+    if (pagination) {
+        const { pageSize } = pagination;
+        pageData = data
+            .filter((_, i) => pageSize * currentPage >= (i + 1) && i >= pageSize * currentPage - pageSize);
+    }
+
     return (
         <div css={styles.tableContainer}>
-            {!props.hideHeaders && (
-                <TableRow header={true} columns={props.columns} styles={styles} />
+            {!hideHeaders && (
+                <TableRow header={true} columns={columns} styles={styles} />
             )}
             <TableBody {...props} data={pageData} styles={styles} />
-            {props.pagination && (
+            {pagination && (
                 <TablePagination
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
-                    dataLength={props.data.length}
+                    dataLength={data.length}
                     styles={styles}
-                    pagination={props.pagination}
+                    pagination={pagination}
                 />
             )}
         </div>
