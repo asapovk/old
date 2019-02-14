12d19a24520e303ebb6822798cf4ce8870abb9f6
Alexander <delle@alexanders-mbp.abrr.local>
2019-02-14 15:01:36 +0300

    build

---
+++ dist/core/SimpleModal/Modal.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("react").ForwardRefExoticComponent<import("react").RefAttributes<{}>>;
+export default _default;
---
+++ dist/core/SimpleModal/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Modal_1 = require("./Modal");
+exports.SimpleModal = Modal_1.default;
