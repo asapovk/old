0ed04ee0d5605cd1254f44a9f1becb57d99850b9
Alexander <delle@alexanders-mbp.abrr.local>
2019-03-04 13:51:21 +0300

    build

--- dist/core/Grid/components/DataRows.js
+++ dist/core/Grid/components/DataRows.js
@@ -4,12 +4,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@emotion/core");
 var react_1 = require("react");
 var _1 = require(".");
+var styles_1 = require("../styles");
 exports.default = (function (props) {
     var groups = props.groups, groupKey = props.groupKey, columns = props.columns, hideHeaders = props.hideHeaders, data = props.data, currentPage = props.currentPage, expandForm = props.expandForm;
-    if (groupKey && (Array.isArray(groups) && groups.length > 0)) {
-        return (core_1.jsx(react_1.Fragment, null, groups.map(function (group, index) { return (core_1.jsx(react_1.Fragment, { key: group.value + "-" + index },
-            core_1.jsx(_1.SubHeader, { key: "sh-" + group.value + "-" + index, title: group.title, columnsLength: columns.length, hideHeaders: hideHeaders }),
-            data.filter(function (row) { return row.groupId === group.value; }).map(function (row, index) { return (core_1.jsx(_1.Row, { key: (JSON.stringify(columns) + group.value.toString() + currentPage.toString() + index).hashCode(), row: row, columns: columns, expandForm: expandForm, onRowClick: props.onRowClick })); }))); })));
+    var styles = styles_1.subHeaderStyles({
+        columnsLength: props.columns.length,
+        hideHeaders: props.hideHeaders
+    });
+    if (groupKey && Array.isArray(groups)) {
+        var uniqueDataGroups_1 = data.filter(function (v, i, a) { return a.indexOf(v) === i; });
+        var currentGroups = groups.filter(function (group) { return uniqueDataGroups_1.some(function (udg) { return udg.groupId === group.value; }); });
+        return (core_1.jsx(react_1.Fragment, null, currentGroups && currentGroups.map(function (group, index) { return (core_1.jsx(react_1.Fragment, { key: group.value + "-" + index },
+            core_1.jsx("div", { key: "sh-" + group.value + "-" + index, css: styles.subheader, children: group.title }),
+            data
+                .filter(function (row) { return row.groupId === group.value; })
+                .map(function (row, index) { return (core_1.jsx(_1.Row, { key: (JSON.stringify(columns) + group.value.toString() + currentPage.toString() + index).hashCode(), row: row, columns: columns, expandForm: expandForm, onRowClick: props.onRowClick })); }))); })));
     }
     return (core_1.jsx(react_1.Fragment, null, data.map(function (row, index) { return (core_1.jsx(_1.Row, { key: (JSON.stringify(columns) + currentPage.toString() + index).hashCode(), row: row, columns: columns, expandForm: expandForm, onRowClick: props.onRowClick })); })));
 });
--- dist/core/Grid/components/index.js
+++ dist/core/Grid/components/index.js
@@ -6,7 +6,5 @@ var DataRows_1 = require("./DataRows");
 exports.DataRows = DataRows_1.default;
 var Header_1 = require("./Header");
 exports.Header = Header_1.default;
-var SubHeader_1 = require("./SubHeader");
-exports.SubHeader = SubHeader_1.default;
 var Pagination_1 = require("./Pagination");
 exports.Pagination = Pagination_1.default;
