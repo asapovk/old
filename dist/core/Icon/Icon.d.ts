0f4c0c2563b073f35452d6d375309394cf6c813e
Dmitry Beltyukov <beltdtr@gmail.com>
2019-04-02 10:56:48 +0300

    build

--- dist/core/Icon/Icon.d.ts
+++ dist/core/Icon/Icon.d.ts
@@ -1,5 +1,4 @@
-import React from 'react';
-import IconTypes from './types';
-declare const _default: React.ForwardRefExoticComponent<IconTypes.Props & React.RefAttributes<{}>>;
+import IconTypes from "./types";
+declare const _default: import("react").ForwardRefExoticComponent<IconTypes.Props & import("react").RefAttributes<{}>>;
 export default _default;
 export declare const svgIconPath: Record<IconTypes.Type, string>;
