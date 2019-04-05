c19a33f5f335ee41012107bd7905d9ab03b3586d
Dmitry Beltyukov <beltdtr@gmail.com>
2019-04-05 12:11:50 +0300

    build

--- dist/core/Widget/styles.js
+++ dist/core/Widget/styles.js
@@ -9,44 +9,42 @@ exports.default = (function (clickable) {
     var theme = useTheme_1.default().theme;
     return {
         container: core_1.css({
-            position: 'relative',
-            boxSizing: 'border-box',
+            position: "relative",
+            boxSizing: "border-box",
             lineHeight: 1,
-            padding: '1rem',
-            minWidth: '16rem',
-            overflow: 'hidden',
+            padding: "1rem",
+            minWidth: "16rem",
+            overflow: "hidden",
             background: theme.interface.rgb,
-            borderRadius: '0.5rem',
-            // borderColor: theme.borders.widget.color,
-            // borderStyle: theme.borders.widget.style,
-            // borderWidth: theme.borders.widget.width,
-            transition: 'all .2s ease',
-            userSelect: 'none',
-            boxShadow: '0 1px 6px 0 rgba(32,33,36,0.28)',
+            borderRadius: "0.5rem",
+            border: "1px solid " + theme.pale.rgb,
+            transition: "all .2s ease",
+            userSelect: "none",
+            boxShadow: theme.shadows.card
         }, clickable && {
             "&:hover": {
-                transform: 'scale(1.05)'
+                transform: "scale(1.05)"
             },
             "&:active": {
-                transform: 'scale(1.03)'
+                transform: "scale(1.03)"
             }
         }),
         title: core_1.css({
-            paddingRight: '1rem',
-            fontSize: '1.125rem',
-            textTransform: 'uppercase'
+            paddingRight: "1rem",
+            fontSize: "1.125rem",
+            textTransform: "uppercase"
         }),
         loading: core_1.css({
-            fontSize: '1.5rem',
+            fontSize: "1.5rem",
             fontWeight: 500,
-            textAlign: 'center',
+            textAlign: "center",
             "> svg": {
                 color: theme.highlight.hex
-            },
+            }
         }),
         loadingText: core_1.css({
-            fontSize: '1rem',
-            paddingTop: '0.75rem',
+            fontSize: "1rem",
+            paddingTop: "0.75rem",
             color: theme.lowlight.hex
         }),
         icon: core_1.css({
