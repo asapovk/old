23bbce51d9bb9f8a955fb42def089c63d64c9baf
Dmitry Beltyukov <beltdtr@gmail.com>
2019-04-05 12:11:25 +0300

    More lines and shadows fixes

--- src/core/Tabs/styles.ts
+++ src/core/Tabs/styles.ts
@@ -1,92 +1,102 @@
-import { css } from '@emotion/core';
-import useTheme from '../../hooks/useTheme';
+import { css } from "@emotion/core";
+import useTheme from "../../hooks/useTheme";
 
 export default ({ reverseContainer }, bp) => {
-    const theme = useTheme().theme;
+  const theme = useTheme().theme;
 
-    const breakpoints = bp ? bp : [414, 768, 1024];
+  const breakpoints = bp ? bp : [414, 768, 1024];
 
+  const mq = breakpoints.map(bp => `@media (max-width: ${bp}px)`);
 
-    const mq = breakpoints.map(
-        bp => `@media (max-width: ${bp}px)`
-    )
+  return {
+    container: css(
+      {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "flex-start"
+      },
+      reverseContainer && {
+        flexDirection: "row-reverse"
+      }
+    ),
 
-    return {
-        container: css({
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-        }, reverseContainer && {
-            flexDirection: 'row-reverse'
-        }),
+    menu: css(
+      {
+        display: "flex",
+        flexDirection: "column",
+        flexBasis: "20rem",
+        flexShrink: 0,
+        flexWrap: "wrap",
+        boxSizing: "border-box",
+        borderStyle: "solid",
+        borderWidth: "1px",
+        borderColor: theme.borders.table.color,
+        borderRadius: "0.5rem",
+        position: "sticky",
+        top: "1.25rem",
+        margin: "0 0 0 2.5rem",
+        padding: "0.25rem 0 0.25rem 1.25rem",
+        [mq[2]]: {
+          display: "none"
+        },
+        "> div:last-of-type > div": {
+          borderWidth: "0"
+        }
+      },
+      reverseContainer && {
+        margin: "0 2.5rem 0 0"
+      }
+    ),
 
-        menu: css({
-            display: 'flex',
-            flexDirection: 'column',
-            flexBasis: '20rem',
-            flexShrink: 0,
-            flexWrap: 'wrap',
-            boxSizing: 'border-box',
-            borderStyle: 'solid',
-            borderWidth: '1px',
-            borderColor: theme.borders.table.color,
-            borderRadius: '0.5rem',
-            position: 'sticky',
-            top: '1.25rem',
-            margin: '0 0 0 2.5rem',
-            padding: '0.25rem 0 0.25rem 1.25rem',
-            [mq[2]]: {
-                display: 'none'
-            },
-            "> div:last-of-type > div": {
-                borderWidth: '0',
-            }
-        }, reverseContainer && {
-            margin: '0 2.5rem 0 0'
-        }),
+    menuItem: (activeItem: boolean) =>
+      css(
+        {
+          display: "block",
+          width: "100%",
+          boxSizing: "border-box",
+          cursor: "pointer",
+          borderWidth: "0 0 1px 0",
+          borderColor: theme.borders.table.color,
+          borderStyle: "solid",
+          padding: "1.25rem 0"
+        },
+        activeItem && {
+          color: theme.highlight.hex
+        }
+      ),
 
-        menuItem: (activeItem: boolean) => css({
-            display: 'block',
-            width: '100%',
-            boxSizing: 'border-box',
-            cursor: 'pointer',
-            borderWidth: '0 0 1px 0',
-            borderColor: theme.borders.table.color,
-            borderStyle: 'solid',
-            padding: '1.25rem 0',
-        }, activeItem && {
-            color: theme.highlight.hex
-        }),
+    content: css({
+      display: "flex",
+      flex: 1,
+      flexWrap: "wrap"
+    }),
 
-        content: css({
-            display: 'flex',
-            flex: 1,
-            flexWrap: 'wrap',
-        }),
+    tab: (activeItem: boolean) =>
+      css(
+        {
+          display: "block",
+          width: "100%",
+          boxSizing: "border-box",
+          borderRadius: "0.5rem",
+          padding: "2rem",
+          marginBottom: "2.5rem",
+          transition: "all .25s ease-in-out",
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderColor: theme.borders.table.color,
+          [mq[0]]: {
+            padding: "1.25rem",
+            marginBottom: "1.25rem"
+          }
+        },
+        activeItem && {
+          boxShadow: theme.shadows.card
+        }
+      ),
 
-        tab: (activeItem: boolean) => css({
-            display: 'block',
-            width: '100%',
-            boxSizing: 'border-box',
-            borderRadius: '0.5rem',
-            padding: '2rem',
-            marginBottom: '2.5rem',
-            transition: 'all .25s ease-in-out',
-            borderStyle: 'solid',
-            borderWidth: '1px',
-            borderColor: theme.borders.table.color,
-            [mq[0]]: {
-                padding: '1.25rem',
-                marginBottom: '1.25rem',
-            },
-        }, activeItem && {
-            boxShadow: '0px .75rem .75rem 0px rgba(0, 0, 0, 0.14)',
-        }),
-
-        noDataContainer: css({
-            padding: '1.25rem',
-            textAlign: 'center'
-        })
-
-    }
-}
+    noDataContainer: css({
+      padding: "1.25rem",
+      textAlign: "center"
+    })
+  };
+};
--- src/core/Widget/styles.ts
+++ src/core/Widget/styles.ts
@@ -1,56 +1,57 @@
-import { css } from '@emotion/core';
-import useTheme from '../../hooks/useTheme';
+import { css } from "@emotion/core";
+import useTheme from "../../hooks/useTheme";
 
 export default (clickable: boolean) => {
-    const theme = useTheme().theme;
-    return {
-        container: css({
-            position: 'relative',
-            boxSizing: 'border-box',
-            lineHeight: 1,
-            padding: '1rem',
-            minWidth: '16rem',
-            overflow: 'hidden',
-            background: theme.interface.rgb,
-            borderRadius: '0.5rem',
-            // borderColor: theme.borders.widget.color,
-            // borderStyle: theme.borders.widget.style,
-            // borderWidth: theme.borders.widget.width,
-            transition: 'all .2s ease',
-            userSelect: 'none',
-            boxShadow: '0 1px 6px 0 rgba(32,33,36,0.28)',
-        }, clickable && {
-            "&:hover": {
-                transform: 'scale(1.05)'
-            },
-            "&:active": {
-                transform: 'scale(1.03)'
-            }
-        }),
+  const theme = useTheme().theme;
+  return {
+    container: css(
+      {
+        position: "relative",
+        boxSizing: "border-box",
+        lineHeight: 1,
+        padding: "1rem",
+        minWidth: "16rem",
+        overflow: "hidden",
+        background: theme.interface.rgb,
+        borderRadius: "0.5rem",
+        border: "1px solid " + theme.pale.rgb,
+        transition: "all .2s ease",
+        userSelect: "none",
+        boxShadow: theme.shadows.card
+      },
+      clickable && {
+        "&:hover": {
+          transform: "scale(1.05)"
+        },
+        "&:active": {
+          transform: "scale(1.03)"
+        }
+      }
+    ),
 
-        title: css({
-            paddingRight: '1rem',
-            fontSize: '1.125rem',
-            textTransform: 'uppercase'
-        }),
+    title: css({
+      paddingRight: "1rem",
+      fontSize: "1.125rem",
+      textTransform: "uppercase"
+    }),
 
-        loading: css({
-            fontSize: '1.5rem',
-            fontWeight: 500,
-            textAlign: 'center',
-            "> svg": {
-                color: theme.highlight.hex
-            },
-        }),
+    loading: css({
+      fontSize: "1.5rem",
+      fontWeight: 500,
+      textAlign: "center",
+      "> svg": {
+        color: theme.highlight.hex
+      }
+    }),
 
-        loadingText: css({
-            fontSize: '1rem',
-            paddingTop: '0.75rem',
-            color: theme.lowlight.hex
-        }),
+    loadingText: css({
+      fontSize: "1rem",
+      paddingTop: "0.75rem",
+      color: theme.lowlight.hex
+    }),
 
-        icon: css({
-            color: theme.highlight.hex
-        })
-    }
-}
+    icon: css({
+      color: theme.highlight.hex
+    })
+  };
+};
