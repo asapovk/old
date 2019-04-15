0a8e9e39fcb1924cf4d07ace228cf7f8db995873
Alexander <delle@alexanders-mbp.abrr.local>
2019-04-15 12:46:36 +0300

    main scene style

--- src/scenes/LKMainScene/styles.ts
+++ src/scenes/LKMainScene/styles.ts
@@ -103,7 +103,7 @@ export default () => {
                 width: '15rem',
                 boxSizing: 'border-box',
                 padding: '2.75rem 0 2.75rem 2.75rem',
-                maxHeight: 'max-content',
+                // maxHeight: 'max-content',
                 minHeight: '100vh',
                 justifyContent: 'space-between',
                 zIndex: 4,
@@ -114,7 +114,7 @@ export default () => {
                 WebkitBackfaceVisibility: 'hidden',
                 WebkitTransform: 'translateZ(0)',
             }, isMobile && {
-                position: 'fixed',
+                position: 'absolute',
                 top: 0,
                 left: 0,
                 bottom: 0,
