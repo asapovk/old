ad03f906adadfb5e276b7387d7e5987119297387
Alexander <delle@alexanders-mbp.abrr.local>
2019-04-24 13:45:38 +0300

    tabs

--- src/core/Tabs/Tabs.tsx
+++ src/core/Tabs/Tabs.tsx
@@ -1,26 +1,23 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { useState } from 'react';
 import { Flexbox } from '..';
-import TabsTypes from './types';
 import createStyles from './styles';
+import TabsTypes from './types';
 
 export default (props: TabsTypes.Props) => {
     const { activeItem, items, className } = props;
-    const [activeItemIndex, setActiveItemIndex] = useState(activeItem || 0);
     const { menu } = createStyles();
 
     return (
         <Flexbox className={className} alignItems='center' mb='2rem'>
             {items.map((item, index) => {
-                const isActive = index === activeItemIndex;
+                const isActive = index === activeItem;
                 return (
                     <Flexbox
                         key={index}
                         css={menu.elementContainer}
                         onClick={() => {
                             item.onClick && item.onClick();
-                            setActiveItemIndex(index);
                         }}
                         children={(
                             <Flexbox
