342ba4f1330133b30ad783c896cee2638570fb53
Alexander <delle@alexanders-mbp.abrr.local>
2019-04-24 13:45:55 +0300

    build

--- dist/core/Tabs/Tabs.js
+++ dist/core/Tabs/Tabs.js
@@ -5,18 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 /** @jsx jsx */
 var core_1 = require("@emotion/core");
-var react_1 = require("react");
 var __1 = require("..");
 var styles_1 = __importDefault(require("./styles"));
 exports.default = (function (props) {
     var activeItem = props.activeItem, items = props.items, className = props.className;
-    var _a = react_1.useState(activeItem || 0), activeItemIndex = _a[0], setActiveItemIndex = _a[1];
     var menu = styles_1.default().menu;
     return (core_1.jsx(__1.Flexbox, { className: className, alignItems: 'center', mb: '2rem' }, items.map(function (item, index) {
-        var isActive = index === activeItemIndex;
+        var isActive = index === activeItem;
         return (core_1.jsx(__1.Flexbox, { key: index, css: menu.elementContainer, onClick: function () {
                 item.onClick && item.onClick();
-                setActiveItemIndex(index);
             }, children: (core_1.jsx(__1.Flexbox, { css: menu.itemContent({ isActive: isActive }), children: item.title })) }));
     })));
 });
