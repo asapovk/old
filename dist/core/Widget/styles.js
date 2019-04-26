c1d337a082e96434d3aa3f32f285b1a5ab0bcca9
Alexander <delle@alexanders-mbp.abrr.local>
2019-04-26 19:20:09 +0300

    build

--- dist/core/Widget/styles.js
+++ dist/core/Widget/styles.js
@@ -19,7 +19,7 @@ var useTheme_1 = __importDefault(require("../../hooks/useTheme"));
 exports.default = (function (clickable, decoration) {
     var theme = useTheme_1.default().theme;
     return {
-        container: core_1.css(__assign({ position: "relative", boxSizing: "border-box", lineHeight: 1, padding: "1rem", minWidth: "16rem", overflow: "hidden", background: theme.interface.rgb, transition: "all .2s ease", userSelect: "none", boxShadow: theme.shadows.widget }, theme.borders.widget), clickable && {
+        container: core_1.css(__assign({ position: "relative", boxSizing: "border-box", lineHeight: 1, padding: "2rem", minWidth: "16rem", overflow: "hidden", background: theme.interface.rgb, transition: "all .2s ease", userSelect: "none", boxShadow: theme.shadows.widget }, theme.borders.widget), clickable && {
             "&:hover": {
                 transform: "scale(1.05)"
             },
