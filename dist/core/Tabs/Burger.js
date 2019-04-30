a2e81dcb72fb2201a60e396defe33f119098c20c
Илья <jleed@me.com>
2019-04-30 17:40:18 +0300

    build

--- dist/core/Tabs/Burger.js
+++ dist/core/Tabs/Burger.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@emotion/core");
 var __1 = require("..");
 exports.default = (function (props) { return (core_1.jsx(__1.Flexbox, { alignItems: "center", onClick: props.onClick },
-    (props.alignMobile === 'flex-end' || props.alignMobile === 'center' || !props.alignMobile) && (core_1.jsx(__1.Flexbox, { css: props.styleTitle }, props.currentMenu.title.toUpperCase())),
+    (props.alignMobile === 'flex-end' || props.alignMobile === 'center' || !props.alignMobile) && props.currentMenu && (core_1.jsx(__1.Flexbox, { css: props.styleTitle }, props.currentMenu.title.toUpperCase())),
     core_1.jsx(__1.Flexbox, { flexDirection: 'column', justifyContent: 'space-between', css: props.styleButton, children: [
             core_1.jsx("div", { key: 1 }),
             core_1.jsx("div", { key: 2 }),
