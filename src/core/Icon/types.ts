58099f28ef8d71d6628f4e5f8fa143ca0478d958
Dmitry Beltyukov <beltdtr@gmail.com>
2019-05-06 13:04:49 +0300

    Lock icon

--- src/core/Icon/types.ts
+++ src/core/Icon/types.ts
@@ -1,67 +1,71 @@
-import React from 'react';
-import { SpacingProps } from '../types';
-import { ThemeInterface } from '../../styles';
+import React from "react";
+import { SpacingProps } from "../types";
+import { ThemeInterface } from "../../styles";
 
 declare namespace IconTypes {
+  export type Type =
+    | "add"
+    | "more"
+    | "pin"
+    | "close"
+    | "search"
+    | "right"
+    | "left"
+    | "down"
+    | "up"
+    | "check"
+    | "sync"
+    | "filter"
+    | "settings"
+    | "chart"
+    | "calendar"
+    | "spin"
+    | "smorodina"
+    | "clock"
+    | "card"
+    | "counter"
+    | "counter_gas"
+    | "counter_water"
+    | "counter_electricity"
+    | "text-right"
+    | "arrow-left"
+    | "user"
+    | "equal"
+    | "photo"
+    | "bell"
+    | "water"
+    | "tv"
+    | "repair"
+    | "electricity"
+    | "insurance"
+    | "other"
+    | "intercom"
+    | "garbage"
+    | "gas"
+    | "heat"
+    | "minus"
+    | "grid"
+    | "list"
+    | "lock"
+    | string;
 
-    export type Type =
-        'add' |
-        'more' |
-        'pin' |
-        'close' |
-        'search' |
-        'right' |
-        'left' |
-        'down' |
-        'up' |
-        'check' |
-        'sync' |
-        'filter' |
-        'settings' |
-        'chart' |
-        'calendar' |
-        'spin' |
-        'smorodina' |
-        'clock' |
-        'card' |
-        'counter' |
-        'counter_gas' |
-        'counter_water' |
-        'counter_electricity' |
-        'text-right' |
-        'arrow-left' |
-        'user' |
-        'equal' |
-        'photo' |
-        'bell' |
-        'water' |
-        'tv' |
-        'repair' |
-        'electricity' |
-        'insurance' |
-        'other' |
-        'intercom' |
-        'garbage' |
-        'gas' |
-        'heat' |
-        'minus' |
-        'grid' |
-        'list' |
-        string
-
-
-    interface Props extends SpacingProps {
-        style?: React.CSSProperties
-        className?: string
-        type: Type
-        onClick?: () => void,
-        shape?: 'cirle' | 'oval',
-        size?: string
-        color?: 'text' | 'highlight' | 'pale' | 'textOnAccent' | 'lowlight' | 'light'
-        // background?: 'text' | 'highlight' | 'pale' | 'textOnAccent' | 'lowlight' | 'light' | 'background' | 'background2'
-        background?: string
-    }
-
+  interface Props extends SpacingProps {
+    style?: React.CSSProperties;
+    className?: string;
+    type: Type;
+    onClick?: () => void;
+    shape?: "cirle" | "oval";
+    size?: string;
+    color?:
+      | "text"
+      | "highlight"
+      | "pale"
+      | "textOnAccent"
+      | "lowlight"
+      | "light";
+    // background?: 'text' | 'highlight' | 'pale' | 'textOnAccent' | 'lowlight' | 'light' | 'background' | 'background2'
+    background?: string;
+  }
 }
 
 export default IconTypes;
