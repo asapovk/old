dc012e77e809f2fc779a0065e0f014a3a6683f24
Alexander <delle@alexanders-mbp.abrr.local>
2019-05-08 11:47:38 +0300

    build

--- dist/styles/jss/Select.js
+++ dist/styles/jss/Select.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (function (theme) {
     return {
-        background: theme.interface.rgb,
+        background: theme.background2.rgb,
         labelColor: theme.lowlight.rgb,
         textColor: theme.text.rgb,
         borderColor: theme.pale.rgb,
