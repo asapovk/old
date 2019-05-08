493f5e79b48dfc867984228e6feebaa7e3988c77
Dmitry Beltyukov <beltdtr@gmail.com>
2019-05-08 10:54:20 +0300

    List showmore icon

--- src/core/List/components/ShowMore.tsx
+++ src/core/List/components/ShowMore.tsx
@@ -1,22 +1,41 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { C2, Flexbox, Icon } from '../..';
-import createStyles from '../styles';
-import Types from '../types';
+import { jsx } from "@emotion/core";
+import { C2, Flexbox, Icon } from "../..";
+import createStyles from "../styles";
+import Types from "../types";
 
 export default (props: Types.MoreContainerProps) => {
-    const styles = createStyles();
-    const { minified, setMinified } = props;
+  const styles = createStyles();
+  const { minified, setMinified } = props;
 
-    let moreLabel = props.moreLabel || 'Раскрыть';
-    let lessLabel = props.lessLabel || 'Скрыть';
+  let moreLabel = props.moreLabel || "Раскрыть";
+  let lessLabel = props.lessLabel || "Скрыть";
 
-    return (
-        <Flexbox mt='2rem' column alignItems='center' justifyContent='center' css={styles.moreContainer}>
-            <C2 mb='0.5rem' color='lowlight' children={minified ? moreLabel : lessLabel} />
-            <Flexbox alignItems='center' justifyContent='center' css={styles.moreButton(minified)}>
-                <Icon size='1.75rem' color='lowlight' type='down' onClick={setMinified} />
-            </Flexbox>
-        </Flexbox>
-    )
-}
+  return (
+    <Flexbox
+      mt="2rem"
+      column
+      alignItems="center"
+      justifyContent="center"
+      css={styles.moreContainer}
+    >
+      <C2
+        mb="0.5rem"
+        color="lowlight"
+        children={minified ? moreLabel : lessLabel}
+      />
+      <Flexbox
+        alignItems="center"
+        justifyContent="center"
+        css={styles.moreButton}
+      >
+        <Icon
+          size="1.75rem"
+          color="lowlight"
+          type={minified ? "down_triangle" : "up_triangle"}
+          onClick={setMinified}
+        />
+      </Flexbox>
+    </Flexbox>
+  );
+};
