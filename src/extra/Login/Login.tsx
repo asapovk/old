221c7fd6feb90b5b76f3352f101c5d7f8e47ed33
Dmitry Beltyukov <beltdtr@gmail.com>
2019-05-14 16:06:47 +0300

    Brand new popup

--- src/extra/Login/Login.tsx
+++ src/extra/Login/Login.tsx
@@ -1,25 +1,27 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { Popup, Icon } from '../../';
-import createStyles from './styles';
-import Types from './types'
+import { jsx } from "@emotion/core";
+import { Popup, Icon } from "../../";
+import createStyles from "./styles";
+import Types from "./types";
 
 export default (props: Types.Props) => {
+  const { name, fullname, login, role, children, avatar } = props;
+  const styles = createStyles();
 
-    const { name, fullname, login, role, children, avatar } = props;
-    const styles = createStyles();
+  function LoginInformation() {
+    return <div>{name}</div>;
+  }
 
-    function LoginInformation() {
-        return <div>{name}</div>
-    }
-
-    return (
-        <Popup
-            trigger={avatar
-                ? <div css={styles.wrapper} children={avatar} />
-                : <Icon type="user" />
-            }
-            children={<LoginInformation />}
-        />
-    )
-}
+  return (
+    <Popup
+      children={
+        avatar ? (
+          <div css={styles.wrapper} children={avatar} />
+        ) : (
+          <Icon type="user" />
+        )
+      }
+      content={<LoginInformation />}
+    />
+  );
+};
