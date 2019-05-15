3f5b396607c88d8201dab466f7e9656e356856bc
Dmitry Beltyukov <beltdtr@gmail.com>
2019-05-15 12:38:58 +0300

    MOAR POPUP IN CASE

--- cases/Core/Popup/index.case.tsx
+++ cases/Core/Popup/index.case.tsx
@@ -41,6 +41,44 @@ class PopupStory extends React.Component {
             <Button label="Trigger Popup" />
           </Popup>
         </Flexbox>
+        <Flexbox
+          css={css({
+            width: "12rem",
+            height: "3rem",
+            overflow: "hidden",
+            background: "yellow"
+          })}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Popup
+            content={<a css={css({ margin: "1rem .5rem" })}>Hello world!</a>}
+            wrapperStyles={css({
+              width: "fit-content"
+            })}
+          >
+            <Button label="Trigger Popup" />
+          </Popup>
+        </Flexbox>
+        <Flexbox
+          css={css({
+            width: "12rem",
+            height: "3rem",
+            overflow: "hidden",
+            background: "blue"
+          })}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Popup
+            content={<a css={css({ margin: "1rem .5rem" })}>Hello world!</a>}
+            wrapperStyles={css({
+              width: "fit-content"
+            })}
+          >
+            <Button label="Trigger Popup" />
+          </Popup>
+        </Flexbox>
         <h1>SCROLL SCROLL SCROLL</h1>
         <h1>SCROLL SCROLL SCROLL</h1>
         <h1>SCROLL SCROLL SCROLL</h1>
