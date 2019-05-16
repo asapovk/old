a91f1949041d2251efd961a94ec13065ef6a002d
Илья <jleed@me.com>
2019-05-16 18:22:03 +0300

    build

--- extra/RublesField/RublesField.d.ts
+++
@@ -1,6 +0,0 @@
-import TextFieldTypes from '../../core/TextField/types';
-declare type IRublesField = Pick<TextFieldTypes.Props, Exclude<keyof TextFieldTypes.Props, 'value'>> & {
-    value: number;
-};
-declare const _default: (props: IRublesField) => JSX.Element;
-export default _default;
